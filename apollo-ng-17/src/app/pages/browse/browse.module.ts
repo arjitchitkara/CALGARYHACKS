fce6e1d4336249a43ee6f21eba3b226c48e25c42
Ahmed-Jawad-Tahmid <105600921+Ahmed-Jawad-Tahmid@users.noreply.github.com>
2025-02-16 03:40:05 -0700

    Update browse.module.ts

--- apollo-ng-17/src/app/pages/browse/browse.module.ts
+++ apollo-ng-17/src/app/pages/browse/browse.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { BrowseComponent } from './browse.components';
+import { BrowseComponent } from './browse.component';
 import { BrowseRoutingModule } from './browse-routing.module';
 import { DataViewModule } from 'primeng/dataview';
 import { PickListModule } from 'primeng/picklist';
