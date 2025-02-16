cbbba12d4f905047ea7e8e68905ec52dc5730428
Siddharth Sadhwani <149826158+fireblaster5392@users.noreply.github.com>
2025-02-16 02:46:18 -0700

    browse

--- apollo-ng-17/src/app/app-routing.module.ts
+++ apollo-ng-17/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AppLayoutComponent } from './layout/app.layout.component';
 import { TrackFormComponent } from './components/track-form/track-form.component';
 import { TrackDashboardComponent } from './components/track-dashboard/track-dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BrowseComponent } from './pages/browse/browse.components';
 
 const routerOptions: ExtraOptions = {
     anchorScrolling: 'enabled'
@@ -24,8 +25,13 @@ const routes: Routes = [
                 path : '',
                 data : { breadcrumb: 'Home'},
                 component : HomeComponent
-            }
+            },
+            {
 
+                path : '',
+                data : { breadcrumb: 'Browse'},
+                component : BrowseComponent
+            }
             // { path: 'testlist',
             // data: { breadcrumb: 'Test List' },
             // component: ListComponent,
